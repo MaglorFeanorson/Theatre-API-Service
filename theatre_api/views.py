e57917b95b248e7b46bba5157443d36b7474fe0e
MaglorFeanorson <daniilrog17@gmail.com>
2024-08-14 20:45:44 +0300

    feat: add filtering and custom serializer logic in PlayViewSet

--- theatre_api/views.py
+++ theatre_api/views.py
@@ -9,6 +9,9 @@ from theatre_api.serializers import (
     PerformanceSerializer,
     ReservationSerializer,
     TheatreHallSerializer,
+    PlayListSerializer,
+    PlayDetailSerializer,
+    PlayImageSerializer,
 )
 
 
@@ -25,6 +28,40 @@ class GenreViewSet(viewsets.ModelViewSet):
 class PlayViewSet(viewsets.ModelViewSet):
     queryset = Play.objects.all()
     serializer_class = PlaySerializer
+
+    @staticmethod
+    def _params_to_ints(queryset):
+        return [int(str_id) for str_id in queryset.split(",")]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        actors = self.request.query_params.get("actors")
+        genre = self.request.query_params.get("genre")
+        title = self.request.query_params.get("title")
+
+        if actors:
+            actors_ids = self._params_to_ints(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        if genre:
+            genre_ids = self._params_to_ints(genre)
+            queryset = queryset.filter(genre__id__in=genre_ids)
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+        if self.action == "upload_image":
+            return PlayImageSerializer
+
+        return PlaySerializer
 
 
 class PerformanceViewSet(viewsets.ModelViewSet):
