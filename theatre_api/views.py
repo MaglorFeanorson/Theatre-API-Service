e4f39ba201b023503ebc8c06426a4238c2c6205a
MaglorFeanorson <daniilrog17@gmail.com>
2024-08-14 20:56:48 +0300

    feat: add and apply custom permission IsAdminOrIfAuthenticatedReadOnly to viewsets

--- theatre_api/views.py
+++ theatre_api/views.py
@@ -14,20 +14,25 @@ from theatre_api.serializers import (
     PlayImageSerializer,
 )
 
+from theatre_api.permissions import IsAdminOrIfAuthenticatedReadOnly
+
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class PlayViewSet(viewsets.ModelViewSet):
     queryset = Play.objects.all()
     serializer_class = PlaySerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     @staticmethod
     def _params_to_ints(queryset):
@@ -67,6 +72,7 @@ class PlayViewSet(viewsets.ModelViewSet):
 class PerformanceViewSet(viewsets.ModelViewSet):
     queryset = Performance.objects.all()
     serializer_class = PerformanceSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
@@ -77,3 +83,4 @@ class ReservationViewSet(viewsets.ModelViewSet):
 class TheatreHallViewSet(viewsets.ModelViewSet):
     queryset = TheatreHall.objects.all()
     serializer_class = TheatreHallSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
