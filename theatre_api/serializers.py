29a009c1e5ead0459215a0cf744bf03d048eeaf7
MaglorFeanorson <daniilrog17@gmail.com>
2024-08-14 18:40:28 +0300

    refactor: add additional fields

--- theatre_api/serializers.py
+++ theatre_api/serializers.py
@@ -20,13 +20,19 @@ class ReservationSerializer(serializers.ModelSerializer):
 class TheatreHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = TheatreHall
-        fields = ("id", "name", "rows", "seats_in_rows")
+        fields = (
+            "id",
+            "name",
+            "rows",
+            "seats_in_rows",
+            "num_seats",
+        )
 
 
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
-        fields = ("id", "first_name", "last_name")
+        fields = ("id", "first_name", "last_name", "full_name", "image")
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -38,7 +44,7 @@ class GenreSerializer(serializers.ModelSerializer):
 class PlaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Play
-        fields = ("id", "title", "description", "genre", "actors")
+        fields = ("id", "title", "description", "genre", "actors", "image")
 
 
 class PerformanceSerializer(serializers.ModelSerializer):
