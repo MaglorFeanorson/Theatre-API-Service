7a40333a3f8b40bba1bc59e046cc9c49a0bca737
MaglorFeanorson <daniilrog17@gmail.com>
2024-08-15 20:58:15 +0300

    refactor: import Django user models in models.py

--- user/models.py
+++ user/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
+from django.contrib.auth.models import (
+    AbstractUser,
+    UserManager as DjangoUserManager
+)
 from django.utils.translation import gettext_lazy as _
 
 
